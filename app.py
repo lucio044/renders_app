003850f46e3564bd98c1c8540fd123f9fa150b5a
lucio <lucio.fernandez.lfc@gmail.com>
2025-03-29 15:44:30 -0500

    actualizacion

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ st.header('ANALISIS EXPLORATORIO DE DATOS DE VEHICULOS')
 if st.button('Construir histograma'):
     st.write("Histograma de la columna  'odometer'")
     if 'odometer' in df.columns:
-            fig = px.histogram(df, x='odometer', title='histograma de odometro')
+        fig = px.histogram(df, x='odometer', title='histograma de odometro')
     else:
-            fig = px.histogram(df, x=df.columns[0], title=f'histograma de  {df.columns[0]}')
+        fig = px.histogram(df, x=df.columns[0], title=f'histograma de {df.columns[0]}')
     st.plotly_chart(fig, use_container_width=True)
 
 # Boton para contruir un grafico de dispersion
 if st.button("construir Scatter Plot"):
     st.write("Grafico de dispersion: Precio vs Odometro")
-    if 'odometer' in df.columns and 'price', title='Precio vs Odometro')
+    if 'odometer' in df.columns and 'price' in df.columns:
         fig2 = px.scatter(df, x='odometer', y='price', title='Precio vs Odometro')
         st.plotly_chart(fig2, use_container_width=True)
     else:
